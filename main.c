d7612abd5387c044f4ad563fdd461f1837e68e7a
thatguy147 <thatguy147@gmail.com>
2014-07-24 16:52:07 +0100

    Update main.c
    Updating to use TCP

--- main.c
+++ main.c
@@ -3,22 +3,39 @@
  
   #include "mbed.h"
   #include "SC16IS750.h"
- 
+  #include <string> 
+  
   SPI spi(p11, p12, p13); //MOSI, MISO, SCK
   SC16IS750_SPI serial_spi(&spi, p8);
   
+  string IP = "192.168.1.11";
+  string port = "5005";
+  string open = "open";
+  
   Serial pc(USBTX,USBRX);
- 
+  
   void initWifly()
-  {
+  {      
         if(serial_spi.connected())
         {
-            serial_spi.writeString("$$$\r");
-            wait(0.5);
+            serial_spi.writeString("\r");
+            serial_spi.writeString("$$$");
+            wait(0.2); 
+            serial_spi.writeString("\r");
             serial_spi.writeString("get everything\r\n");
+            wait(0.2);
+            const string openConnection = (open + " " + IP + " "+ port +"\r");
+            char * writable = new char[openConnection.size() + 1];
+            serial_spi.writeString(writable); 
         }
-    }
-    
+   } 
+   
+   void sendSomething()
+   {
+       serial_spi.writeString("\r\rSendingSomething");
+       pc.printf("SendingSomethingOnSerial");
+   }    
+
     void terminal()
     {
         while(1)
@@ -30,15 +47,26 @@
         
             if(pc.readable())
             {
-                serial_spi.putc(pc.getc());
+                if(pc.getc() == ']')
+                {
+                    sendSomething();
+                }
+                else{
+                    serial_spi.putc(pc.getc());
+                }   
             }
+              
         }
     }
-    
+
     int main()
     {
+        
         pc.printf("\nHello World!\n");
         initWifly();
         terminal();
-    
+        //while(1)
+        {
+        //    sendSomething();
+        }
     }
